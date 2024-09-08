6ff67b5d297745badd78d21bc36c67b98bf511b2
Dimuthu-bn <dimuthu.dhanushka@booknordics.com>
2024-09-08 18:51:23 +0530

    Add factory

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,34 +1,18 @@
-﻿// Builder Example usage
-using DesignPatterns;
+﻿using DesignPatterns;
 
+// Usage Example
 class Program
 {
     static void Main(string[] args)
     {
-        // Building a custom computer using the builder pattern
-        // Computer 2
-        Computer myComputer = new Computer.ComputerBuilder("Intel i9", "NVIDIA RTX 3080", 32, 1024)
-            .AddWiFi(true)
-            .AddBluetooth(true)
-            .Build();
+        VehicleFactory factory = new VehicleFactory();
 
-        Console.WriteLine(myComputer);
+        // Creating a car
+        IVehicle vehicle1 = factory.GetVehicle("car");
+        vehicle1.Drive();  // Output: Driving a car!
 
-
-        Console.WriteLine("--------------------");
-
-
-        // Building a custom computer using the builder pattern
-        // Computer 2
-        Computer myComputer2 = new Computer.ComputerBuilder("Intel i7", "NVIDIA RTX 3080", 32, 1024)
-            .AddWiFi(true)
-            .Build();
-
-        Console.WriteLine(myComputer2);
-
-
-        // If you don't use the pattern you will end up with longer lists of parameters.
-        // Computer computer3 = new Computer("Intel i7", "NVIDIA RTX 3080", 32, 1024, true, true)
-
+        // Creating a bike
+        IVehicle vehicle2 = factory.GetVehicle("bike");
+        vehicle2.Drive();  // Output: Riding a bike!
     }
 }
---
+++ DesignPatterns/VehicleFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    using System;
+
+    // Product Interface
+    public interface IVehicle
+    {
+        void Drive();
+    }
+
+    // Concrete Product Classes
+    public class Car : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving a car!");
+        }
+    }
+
+    public class Bike : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Riding a bike!");
+        }
+    }
+
+    // Factory Class
+    public class VehicleFactory
+    {
+        // Factory method to create a vehicle
+        public IVehicle GetVehicle(string vehicleType)
+        {
+            switch (vehicleType.ToLower())
+            {
+                case "car":
+                    return new Car();
+                case "bike":
+                    return new Bike();
+                default:
+                    throw new ArgumentException("Unknown vehicle type.");
+            }
+        }
+    }
+
+
+
+}
