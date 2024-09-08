a02b274d0f6eae0140d51fb255df49a461c98a04
Dimuthu-bn <dimuthu.dhanushka@booknordics.com>
2024-09-08 18:45:52 +0530

    add builder

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -25,5 +25,10 @@ class Program
             .Build();
 
         Console.WriteLine(myComputer2);
+
+
+        // If you don't use the pattern you will end up with longer lists of parameters.
+        // Computer computer3 = new Computer("Intel i7", "NVIDIA RTX 3080", 32, 1024, true, true)
+
     }
 }
